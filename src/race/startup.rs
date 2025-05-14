36dbcc81d4432662a7b907c19deeac4afaf24b0b
Juniper <67175453+Ultrasquid9@users.noreply.github.com>
2025-05-14 12:07:23 -0400

    Show Countdown

--- src/race/startup.rs
+++ src/race/startup.rs
@@ -1,36 +1,37 @@
-use kira::sound::{static_sound::StaticSoundHandle, PlaybackState};
+use kira::sound::{PlaybackState, static_sound::StaticSoundHandle};
 use macroquad::prelude::*;
 
 use crate::audio::play_or_load;
 
 pub struct Startup {
 	handle: StaticSoundHandle,
-	countdown_pos: Vec2,
-	countdown_size: Vec2,
-	countdown_texture: Texture2D,
+	countdown: Countdown,
 	gate_pos: Vec2,
 	gate_size: Vec2,
 	gate_texture: Texture2D,
 }
 
+struct Countdown {
+	pos: Vec2,
+	size: Vec2,
+	texture: Texture2D,
+	direction: u8,
+}
+
 impl Startup {
 	pub async fn new() -> Self {
-		let x = Self {
+		Self {
 			handle: play_or_load("../assets/audio/place-your-bets-in.flac"),
-			countdown_pos: vec2(0., 0.),
-			countdown_size: vec2(0., 0.),
-			countdown_texture: Texture2D::from_image(
-				&load_image("./assets/gate.png").await.unwrap(),
-			),
+			countdown: Countdown::new().await,
 			gate_pos: vec2(0., 0.),
 			gate_size: vec2(0., 0.),
 			gate_texture: Texture2D::from_image(&load_image("./assets/gate.png").await.unwrap()),
-		};
-		println!("done");
-		x
+		}
 	}
 
-	pub fn update(&mut self) {}
+	pub fn update(&mut self) {
+		self.countdown.update();
+	}
 
 	pub fn draw(&self) {
 		draw_texture_ex(
@@ -44,19 +45,66 @@ impl Startup {
 			},
 		);
 
-		draw_texture_ex(
-			&self.countdown_texture,
-			self.countdown_pos.x,
-			self.countdown_pos.y,
-			WHITE,
-			DrawTextureParams {
-				dest_size: Some(self.countdown_size),
-				..Default::default()
-			},
-		);
+		self.countdown.draw();
 	}
 
 	pub fn done(&self) -> bool {
 		matches!(self.handle.state(), PlaybackState::Stopped)
+	}
+}
+
+impl Countdown {
+	const DIRS: [Vec2; 4] = [
+		vec2(24., 24.),
+		vec2(-24., 24.),
+		vec2(-24., -24.),
+		vec2(24., -24.),
+	];
+
+	async fn new() -> Self {
+		Self {
+			pos: vec2(screen_width() / 2., screen_height() / 2.),
+			size: vec2(screen_width() / 25., screen_height() / 10.),
+			texture: Texture2D::from_image(&load_image("./assets/gate.png").await.unwrap()),
+			direction: 0,
+		}
+	}
+
+	fn update(&mut self) {
+		self.size();
+		self.bounce();
+	}
+
+	fn bounce(&mut self) {
+		if self.pos.x <= 0.
+			|| self.pos.y <= 0.
+			|| self.pos.x + self.size.x >= screen_width()
+			|| self.pos.y + self.size.y >= screen_height()
+		{
+			self.direction += 1;
+			if self.direction >= 4 {
+				self.direction = 0;
+			}
+		}
+
+		let dir = Self::DIRS[self.direction as usize];
+		self.pos += dir * get_frame_time() * 4.;
+	}
+
+	fn size(&mut self) {
+		self.size = vec2(screen_width() / 4., screen_height() / 14.)
+	}
+
+	fn draw(&self) {
+		draw_texture_ex(
+			&self.texture,
+			self.pos.x,
+			self.pos.y,
+			WHITE,
+			DrawTextureParams {
+				dest_size: Some(self.size),
+				..Default::default()
+			},
+		);
 	}
 }
